9ef9ef1fb1b054d3faebbca40431eeb0984b4b86
Ksenia Slyshinskaya <ksenia_slys@mail.ru>
2023-03-23 16:22:18 +0300

    Написали второй метод

--- Program.cs
+++ Program.cs
@@ -17,3 +17,11 @@ void NewArray(string [] array, string [] array2)
     }
 }
 
+void PrintArray (string [] array)
+{
+    for (int i = 0; i <array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+   
+}
