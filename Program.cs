31c3ceda1f94cff91dfcf3abdadb975dc8872907
Ksenia Slyshinskaya <ksenia_slys@mail.ru>
2023-03-23 16:21:47 +0300

    Написали первый метод

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.Clear();
+string [] array = new string[5] {"123", "hello", "res", "67", "world"};
+string [] array2 = new string[array.Length];
+NewArray(array, array2);
+PrintArray(array2);
+void NewArray(string [] array, string [] array2)
+{
+    int count = 0;
+    for (int i = 0; i <array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+        array2[count] = array[i];
+        count ++;
+        }
+
+    }
+}
+
